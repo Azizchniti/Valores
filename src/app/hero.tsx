a650e02a73a92ee7dc72036435d9873a8734586d
Aziz <chnitiaziz@gmail.com>
2026-03-05 10:19:47 -0300

    video not appearing on all browsers fixed

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -4,34 +4,24 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <section
-      className="
-        relative
-        w-full
-        h-[60vh] sm:h-[70vh] lg:h-[90vh]
-        overflow-hidden
-        bg-black
-      "
-    >
-      {/* Background video */}
+    <section className="relative w-full h-[60vh] sm:h-[70vh] lg:h-[90vh] overflow-hidden bg-black">
+      
       <video
-        src="https://res.cloudinary.com/ddewzsm7q/video/upload/v1761913696/Valores/Video_hero_njtlhi.mp4"
         autoPlay
         muted
         loop
         playsInline
-        className="
-          absolute
-          inset-0
-          w-full
-          h-full
-          object-cover
-    
-        "
-      />
+        preload="auto"
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source
+          src="https://res.cloudinary.com/ddewzsm7q/video/upload/f_auto,q_auto/v1761913696/Valores/Video_hero_njtlhi"
+          type="video/mp4"
+        />
+      </video>
 
-      {/* Gradient overlay */}
       <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-[#000409] to-transparent" />
+      
     </section>
   );
 };
